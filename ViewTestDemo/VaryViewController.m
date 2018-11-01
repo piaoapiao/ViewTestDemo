40261d08212cf5e4174308c236e62d7e36595f33
guodong <>
2018-11-01 15:09:10 +0800

    test vary trait

---
+++ ViewTestDemo/VaryViewController.m
@@ -0,0 +1,32 @@
+//
+//  VaryViewController.m
+//  ViewTestDemo
+//
+//  Created by guodong on 2018/11/1.
+//  Copyright © 2018年 ViewTest. All rights reserved.
+//
+
+#import "VaryViewController.h"
+
+@interface VaryViewController ()
+
+@end
+
+@implementation VaryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
